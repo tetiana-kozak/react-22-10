79e531b5f7710f75cf999f0a581aa9c1ac32d505
Tetiana <kozaktanya077@gmail.com>
2023-01-22 11:25:13 +0200

    add some content in index.jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// without jsx
+// const title = React.createElement('h1', {id: 'title'}, "hello react js")
+// console.log(title);
+
+// jsx => js extended
+const title = <h1 id="title">Hello js</h1>
+console.log(title)
+
+const content = (
+    <div>
+        {title}
+        {10 + 10}
+        <ul className="list">
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ul>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
