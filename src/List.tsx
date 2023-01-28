6ed9ea291ac3ea6871bfc22cf9f4ba099128de75
Tetiana <kozaktanya077@gmail.com>
2023-01-28 10:32:13 +0200

    add css with using variable

--- src/List.tsx
+++ src/List.tsx
@@ -2,6 +2,7 @@
 
 type Props = {}
 export const List = (props: Props) => {
+    let data = true
     return (
         <>
             <ul>
@@ -14,6 +15,17 @@ export const List = (props: Props) => {
                 Corrupti labore tenetur nemo perferendis, ut atque incidunt!
                 Neque officia maxime, sed aperiam quod ratione similique
                 veritatis nemo dolor exercitationem voluptas odit.
+            </p>
+            <p
+                style={{
+                    color: 'white',
+                    backgroundColor: data ? 'purple' : 'tomato',
+                }}
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                accusantium sed quod a animi tempora magni dolore mollitia iusto
+                consequatur ipsum voluptate, atque suscipit, inventore facere,
+                nihil maiores officiis libero?
             </p>
         </>
     )
