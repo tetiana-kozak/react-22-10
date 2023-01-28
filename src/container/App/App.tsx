4072912e4aa92e50c70341ba687767d0473a30b5
Tetiana <kozaktanya077@gmail.com>
2023-01-28 11:40:51 +0200

    add basic folders to project and import it

---
+++ src/container/App/App.tsx
@@ -0,0 +1,15 @@
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+type Props = {}
+const App = (props: Props) => {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+export default App
