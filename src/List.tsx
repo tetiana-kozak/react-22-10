a5a41bebafd5230fde45cc92d47b0529d61bc333
Tetiana <kozaktanya077@gmail.com>
2023-01-28 10:39:58 +0200

    add style using import

--- src/List.tsx
+++ src/List.tsx
@@ -1,8 +1,9 @@
 // tsrafce
 
+import './List.css'
+
 type Props = {}
 export const List = (props: Props) => {
-    let data = true
     return (
         <>
             <ul>
@@ -10,18 +11,13 @@ export const List = (props: Props) => {
                 <li>List item 20</li>
                 <li>List item 30</li>
             </ul>
-            <p>
+            <p className="content">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Corrupti labore tenetur nemo perferendis, ut atque incidunt!
                 Neque officia maxime, sed aperiam quod ratione similique
                 veritatis nemo dolor exercitationem voluptas odit.
             </p>
-            <p
-                style={{
-                    color: 'white',
-                    backgroundColor: data ? 'purple' : 'tomato',
-                }}
-            >
+            <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
                 accusantium sed quod a animi tempora magni dolore mollitia iusto
                 consequatur ipsum voluptate, atque suscipit, inventore facere,
