4373ef9f9e725ea296df1130f912c37ff2f810c2
Tetiana <kozaktanya077@gmail.com>
2023-01-29 10:52:49 +0200

    Add header using Material library

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,12 +1,14 @@
 import Footer from "container/Footer/Footer"
 import Header from "container/Header/Header"
 import Main from "container/Main/Main"
+import CssBaseline from '@mui/material/CssBaseline'
 
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
