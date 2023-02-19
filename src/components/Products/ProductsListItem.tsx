350bfb9e3e0084cd2399b7dcdafa5c08cc7141bf
Tetiana <kozaktanya077@gmail.com>
2023-02-19 10:50:53 +0200

    add changeColor functionality

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -25,12 +25,17 @@ const ProductsListItem = ({
     image,
 }: Props) => {
     const [count, setCount] = useState<number>(1)
+    const [color, setColor] = useState<string>('green')
 
     const onIncrement = (num: number) => {
         setCount((prevState) => prevState + num)
     }
     const onDecrement = () => {
         setCount((prevState) => prevState - 1)
+    }
+
+    const ChangeColor = () => {
+        setColor((prevState) => (prevState === 'green' ? 'red' : 'green'))
     }
 
     return (
@@ -43,6 +48,12 @@ const ProductsListItem = ({
                 <div className="product-description">This is {description}</div>
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity} Gb</div>
+
+                <div>
+                    Color: <span className={color}>{color}</span>{' '}
+                </div>
+                <button onClick={() => ChangeColor()}>Change color</button>
+
                 <div className="product-price">{price}$</div>
                 <div className="product-quantity">
                     <Button
