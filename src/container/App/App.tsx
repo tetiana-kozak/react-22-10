362e3569c6c558c250dd42461d391a49fba7961f
Tetiana <kozaktanya077@gmail.com>
2023-02-20 22:45:14 +0200

    send parameters from App to CartHeader

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,16 +1,25 @@
-import Footer from "container/Footer/Footer"
-import Header from "container/Header/Header"
-import Main from "container/Main/Main"
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
-
+import { useState } from 'react'
 
 type Props = {}
+type CartDataProps = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = (props: Props) => {
+    const [cartData, setCartData] = useState<CartDataProps>({
+        totalCount: 0,
+        totalPrice: 0,
+    })
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header />
+            <Header cartData={cartData} />
             <Main />
             <Footer />
         </StyledEngineProvider>
