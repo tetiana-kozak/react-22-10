de300ca492a839fc76842013148d9cf29595b9e3
Tetiana <kozaktanya077@gmail.com>
2023-02-25 11:50:29 +0200

    add productsInCart

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -6,26 +6,27 @@ import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
 
 type Props = {}
-type CartDataProps = {
-    totalCount: number
-    totalPrice: number
+
+type productsInCartType = {
+    [id: number]: number
 }
 
 const App = (props: Props) => {
-    const [cartData, setCartData] = useState<CartDataProps>({
-        totalCount: 0,
-        totalPrice: 0,
+    const [productsInCart, setProductsInCart] = useState<productsInCartType>({
+        1: 5,
+        2: 5,
     })
+
     const addProductToCart = (count: number, price: number) => {
-        setCartData((prevState) => ({
-            totalCount: prevState.totalCount + count,
-            totalPrice: prevState.totalPrice + count * price,
-        }))
+        // setCartData((prevState) => ({
+        //     totalCount: prevState.totalCount + count,
+        //     totalPrice: prevState.totalPrice + count * price,
+        // }))
     }
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header cartData={cartData} />
+            <Header productsInCart={productsInCart} />
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
