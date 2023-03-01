256f25ac26802fa4637abb5374ecd0b0c37381ec
Tetiana <kozaktanya077@gmail.com>
2023-03-01 21:59:13 +0200

    chore

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -28,7 +28,6 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            {/* <button onClick={() => addProductToCart(2, 5)}>Add to cart</button> */}
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
