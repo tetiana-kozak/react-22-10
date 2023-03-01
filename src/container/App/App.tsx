0ef10b2658bdfc59ebfc27fcdbc5e54725266768
Tetiana <kozaktanya077@gmail.com>
2023-03-01 23:36:52 +0200

    add basic react-router elements

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,12 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
-import Main from 'container/Main/Main'
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import CartPage from 'pages/CartPage/CartPage'
 
 type Props = {}
 
@@ -25,7 +28,15 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <Main addProductToCart={addProductToCart} />
+            <Container sx={{ padding: '60px 0' }}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Container>
             <Footer />
         </StyledEngineProvider>
     )
---
+++ src/pages/CartPage/CartPage.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+
+const CartPage = (props: Props) => {
+    return (
+        <div>
+            <Typography variant="h4" component="h1">
+                Cart
+            </Typography>
+        </div>
+    )
+}
+export default CartPage
