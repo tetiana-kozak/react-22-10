49f0b34b807c6a854a2e5662a4447556bd2f5bdf
Tetiana <kozaktanya077@gmail.com>
2023-03-02 23:28:50 +0200

    add some styles to cart

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
 import productsArray, { getProductsObject, Product } from 'utils/productsArray'
 import { Typography } from '@mui/material'
+import './CartPage.scss'
 
 type Props = {
     productsInCart: {
@@ -16,33 +17,56 @@ const CartPage = ({
 }: Props) => {
     return (
         <div>
-            <Typography variant="h4" component="h1">
+            <Typography
+                variant="h4"
+                component="h1"
+                sx={{
+                    marginBottom: '30px',
+                    textAlign: 'center',
+                }}
+            >
                 Cart
             </Typography>
             <div>
                 <div>
                     {Object.keys(productsInCart).map((productId) => (
-                        <div key={productId}>
+                        <div key={productId} className="cart-card">
                             <div>
-                                {productsObject[parseInt(productId)].title} :{' '}
-                                {productsInCart[parseInt(productId)]}
+                                <div>
+                                    {productsObject[parseInt(productId)].title}{' '}
+                                    : {productsInCart[parseInt(productId)]}
+                                </div>
+                                <div className="price">
+                                    Price per product :{' '}
+                                    {productsObject[parseInt(productId)].price}{' '}
+                                    $
+                                </div>
                             </div>
                             <div>
-                                Price per product :{' '}
-                                {productsObject[parseInt(productId)].price} $
+                                <div className="count">
+                                    count: {productsInCart[parseInt(productId)]}
+                                </div>
+                                <div className="sum">
+                                    {productsObject[parseInt(productId)].price *
+                                        productsInCart[
+                                            parseInt(productId)
+                                        ]}{' '}
+                                    $
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
-                <div>
-                    Total: $
+                <div className="cart-total">
+                    Total:{' '}
                     {Object.keys(productsInCart).reduce(
                         (total, productId) =>
                             total +
                             productsObject[parseInt(productId)].price *
                                 productsInCart[parseInt(productId)],
                         0
-                    )}
+                    )}{' '}
+                    $
                 </div>
             </div>
         </div>
