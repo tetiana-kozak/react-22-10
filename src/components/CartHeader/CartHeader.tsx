84d29e7bb3776c3d537c614f346bbe3091bcda2e
Tetiana <kozaktanya077@gmail.com>
2023-03-02 23:39:20 +0200

    add product counter to header

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,4 +1,5 @@
 import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+import './CartHeader.scss'
 
 type Props = {
     productsInCart: {
@@ -14,21 +15,10 @@ const CartHeader = ({
 }: Props) => {
     return (
         <div>
-            <div>
-                {Object.keys(productsInCart).map((productId) => (
-                    <div key={productId}>
-                        {productsObject[parseInt(productId)].title} :{' '}
-                        {productsInCart[parseInt(productId)]}
-                    </div>
-                ))}
-            </div>
-            <div>
-                Total: $
+            <div className="header-total-count">
                 {Object.keys(productsInCart).reduce(
                     (total, productId) =>
-                        total +
-                        productsObject[parseInt(productId)].price *
-                            productsInCart[parseInt(productId)],
+                        total + productsInCart[parseInt(productId)],
                     0
                 )}
             </div>
