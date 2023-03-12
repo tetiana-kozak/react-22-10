342ae0b8415484cd5db0422097bf4df0af19693b
Tetiana <kozaktanya077@gmail.com>
2023-03-12 11:11:26 +0200

    add hooks.ts file

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,3 +6,6 @@ export const store = configureStore({
         productsLike: likeReducer,
     },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
