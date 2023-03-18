c8568509ca8510eb9a62b85ff075616b63644e3c
Tetiana <kozaktanya077@gmail.com>
2023-03-18 10:44:03 +0200

    add likes in cart

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -2,6 +2,10 @@ import { Card, CardContent, Grid, Button } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Quantity from 'components/Quantity/Quantity'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
+import { toggleLike } from 'redux/likeReducer'
 
 type Props = {
     productCount: number
@@ -16,10 +20,19 @@ const CartProductListItemExtended = ({
     removeProductFromCart,
     changeProductQuantity,
 }: Props) => {
+    const isLiked = useAppSelector((state) => state.productsLike[product.id])
+    const dispatch = useAppDispatch()
+
     return (
         <Grid item xs={12} sm={4}>
             <Card variant="outlined">
                 <CardContent>
+                    <Button
+                        variant="outlined"
+                        onClick={() => dispatch(toggleLike(product.id))}
+                    >
+                        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                    </Button>
                     <div className="product-image">
                         <img src={product.image} alt="" />
                     </div>
